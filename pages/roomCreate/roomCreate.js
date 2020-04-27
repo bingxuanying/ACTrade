141a990d0a4dc822513759ac9cb2ceee9d360b9e
Bingxuan Ying <yingbingxuan@gmail.com>
2020-04-27 15:16:09 -0700

    Create room and add to db

--- pages/roomCreate/roomCreate.js
+++ pages/roomCreate/roomCreate.js
@@ -19,16 +19,17 @@ Page({
     })
   },
   bindPriceInput: function(e) {
-    console.log('price: ' + e.detail.value)
+    var priceAdjust = e.detail.value.replace(/\D+/g, '')
     this.setData({
-      price: e.detail.value
+      price: priceAdjust ? parseInt(priceAdjust, 10) : 0
     })
+    console.log('price: ' + this.data.price)
   },
   bindCodeInput: function(e) {
-    console.log('code: ' + e.detail.value)
     this.setData({
-      code: e.detail.value
+      code: e.detail.value.toUpperCase()
     })
+    console.log('code: ' + this.data.code)
   },
   bindSliderTime: function(e) {
     console.log('time: ' + e.detail.value)
@@ -53,6 +54,26 @@ Page({
       complete: (res) => {
         console.log(res)
       },
+    })
+  },
+  onTapCreate: function() {
+    const db = wx.cloud.database();
+    var dbName = this.data.flight === 0 ? 'BusinessFlights' : 'PrivateFlights';
+    var roomNum = Math.floor(Math.random() * 1000).toString();
+
+    db.collection(dbName).add({
+      data: {
+        flight: this.data.flight,
+        price: this.data.price,
+        code: this.data.code,
+        time: this.data.time,
+        people: this.data.people,
+        note: this.data.note,
+        roomNum: '0'.repeat(3 - roomNum.length) + roomNum
+      },
+      success: function(res) {
+        console.log(res)
+      }
     })
   }
 })
