b8c96aa642de846c1705e274ba747faef536c892
CharlesYWL <xlyin@ucdavis.edu>
2020-05-02 23:06:11 -0700

    fix green circle phase problem

--- app.js
+++ app.js
@@ -3,72 +3,71 @@ App({
   onLaunch: function () {
     // Init cloud env
     wx.cloud.init({
-      env: "vegi-exchange-45j4z"
-    })
+      env: "vegi-exchange-45j4z",
+    });
 
     // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    var logs = wx.getStorageSync("logs") || [];
+    logs.unshift(Date.now());
+    wx.setStorageSync("logs", logs);
 
     // 登录
     wx.login({
-      success: res => {
+      success: (res) => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
+      },
+    });
     // 获取用户信息
     wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
+      success: (res) => {
+        if (res.authSetting["scope.userInfo"]) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
-            success: res => {
+            success: (res) => {
               // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
+              this.globalData.userInfo = res.userInfo;
 
               const db = wx.cloud.database();
-              db.collection('UsersProfile')
-              .get({
-                success: res => {
-                  if (res.data.length > 0){
-                    this.globalData.id = res.data[0]._id
+              db.collection("UsersProfile").get({
+                success: (res) => {
+                  if (res.data.length > 0) {
+                    this.globalData.id = res.data[0]._id;
                     this.globalData.gameProfile = {
                       nickname: res.data[0].nickname,
                       islandName: res.data[0].islandName,
                       fruit: res.data[0].fruit,
                       hemisphere: res.data[0].hemisphere,
-                    }
+                    };
                   }
                 },
-                fail: res => {
-                  console.log('fail: ' + res)
-                }
-              })
+                fail: (res) => {
+                  console.log("fail: " + res);
+                },
+              });
 
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
               if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
+                this.userInfoReadyCallback(res);
               }
-            }
-          })
+            },
+          });
         }
-      }
-    })
+      },
+    });
   },
   globalData: {
     id: null,
     userInfo: null,
     gameProfile: {
-      nickname: '',
-      islandName: '',
-      fruit: 'apple',
-      hemisphere: 'north',
+      nickname: "",
+      islandName: "",
+      fruit: "apple",
+      hemisphere: "north",
     },
     roomInfo: {
-      roomID: 'f10018335eab718600317a395487a68a', //f10018335eab718600317a395487a68a,
-      timeStamp: null
-    }
-  }
-})
+      roomID: "f10018335eab718600317a395487a68a", //f10018335eab718600317a395487a68a,
+      timeStamp: null,
+    },
+  },
+});
