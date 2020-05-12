c076cd6c5a8ed8a620c53748792561089553d364
CharlesYWL <xlyin@ucdavis.edu>
2020-05-12 07:21:59 -0700

    load local json file to speedup img load time

---
+++ utils/imgUrl.js
@@ -0,0 +1,21 @@
+export default
+{
+  "imgUrl":{
+    "tradingFloor":{
+      "InfoPageBrown_in": "https://7665-vegi-exchange-45j4z-1301890684.tcb.qcloud.la/dev/img/InfoPageBrown_in.png",
+      "InfoPageRed_in": "https://7665-vegi-exchange-45j4z-1301890684.tcb.qcloud.la/dev/img/InfoPageRed_in.png",
+      "InfoPageYellow_in":"https://7665-vegi-exchange-45j4z-1301890684.tcb.qcloud.la/dev/img/InfoPageYellow_in.png"
+    },
+    "nookea":{},
+    "profile":{},
+    "roomCreate":{},
+    "roomMaster":{},
+    "roomSlave":{},
+    "gif":{
+      "EarthLoading":"https://7665-vegi-exchange-45j4z-1301890684.tcb.qcloud.la/dev/gif/EarthLoading.gif",
+      "FlightLoading":"https://7665-vegi-exchange-45j4z-1301890684.tcb.qcloud.la/dev/gif/FlightLoading.gif",
+      "IslandLoading":"https://7665-vegi-exchange-45j4z-1301890684.tcb.qcloud.la/dev/gif/IslandLoading.gif",
+      "PassportLoading":"https://7665-vegi-exchange-45j4z-1301890684.tcb.qcloud.la/dev/gif/PassportLoading.gif"
+    }
+  }
+}
