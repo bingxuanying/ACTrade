d7f6d78785f6db96d3e253c8cd7304e7f6fc4c37
CharlesYWL <xlyin@ucdavis.edu>
2020-05-14 02:36:49 -0700

    add func to imgurlGetter

--- cloud/imgurlGetter/index.js
+++ cloud/imgurlGetter/index.js
@@ -5,19 +5,36 @@ const db = cloud.database();
 const _ = db.command;
 exports.main = async (event, context) => {
   try {
-    return await cloud.getTempFileURL({
-      fileList: [
-        'cloud://vegi-exchange-45j4z.7665-vegi-exchange-45j4z-1301890684/dev/gif/EarthLoading.gif',
-        'cloud://vegi-exchange-45j4z.7665-vegi-exchange-45j4z-1301890684/dev/gif/FlightLoading.gif',
-        'cloud://vegi-exchange-45j4z.7665-vegi-exchange-45j4z-1301890684/dev/gif/IslandLoading.gif',
-        'cloud://vegi-exchange-45j4z.7665-vegi-exchange-45j4z-1301890684/dev/gif/PassportLoading.gif'
-      ]
-    }).then(res => {
-      // get temp file URL
-      console.log(res.fileList)
-    }).catch(error => {
-      console.log(err)
-    })
+    // 获取imgurl的方程
+    // return await cloud.getTempFileURL({
+    //   fileList: [
+    //     'cloud://vegi-exchange-45j4z.7665-vegi-exchange-45j4z-1301890684/dev/nookea/DiyCollection.png',
+    //   ]
+    // }).then(res => {
+    //   // get temp file URL
+    //   console.log(res.fileList)
+    // }).catch(error => {
+    //   console.log(err)
+    // })
+
+    //一次性添加多条collection的方程
+    var pms = []
+    var i = 0, max = 5
+    const name = 'Plants'
+    while(i < max){
+      console.log(i)
+      let p = db.collection(name).add({
+        data:{
+          "category":"灌木",
+          "type":"",
+          "name":"",
+          "img_url":"",
+        }
+      }) 
+      pms.push(p)
+      i = i + 1
+    }
+    return Promise.all(pms);
   } catch (e) {
     console.log(e)
   }
